6571074c49ecc75f613f99bbfb0a204f266644b6
Antoine De Cock <34279257+spartoine118@users.noreply.github.com>
2023-11-20 12:45:50 +0700

    More changes

--- my-app/src/store/store.ts
+++ my-app/src/store/store.ts
@@ -1,75 +1,75 @@
-import { Action } from "../types";
-import {
-  COUNTER_KEY,
-  CounterPartialState,
-  CounterState,
-  counterReducer,
-} from "./counterSlice/counterReducer";
+// import { Action } from "../types";
+// import {
+//   COUNTER_KEY,
+//   CounterPartialState,
+//   CounterState,
+//   counterReducer,
+// } from "./counterSlice/counterReducer";
 
-export interface GlobalState {
-  [COUNTER_KEY]: CounterState;
-}
+// export interface GlobalState {
+//   [COUNTER_KEY]: CounterState;
+// }
 
-export type UnaryFunction<T, R> = (value: T) => R;
+// export type UnaryFunction<T, R> = (value: T) => R;
 
-export type Reducer<TState, TInput> = (state: TState, input: TInput) => TState;
+// export type Reducer<TState, TInput> = (state: TState, input: TInput) => TState;
 
-export type ReducerDictionary<
-  TState extends Record<K, unknown>,
-  K extends string | number | symbol = keyof TState
-> = Record<K, Reducer<TState[K], Action>>;
+// export type ReducerDictionary<
+//   TState extends Record<K, unknown>,
+//   K extends string | number | symbol = keyof TState
+// > = Record<K, Reducer<TState[K], Action>>;
 
-export class Store<
-  TState extends Record<K, unknown>,
-  K extends string | number | symbol = keyof TState
-> {
-  private state: TState;
-  private reducers: ReducerDictionary<TState>;
-  private listeners = new Set<UnaryFunction<TState, void>>();
+// export class Store<
+//   TState extends Record<K, unknown>,
+//   K extends string | number | symbol = keyof TState
+// > {
+//   private state: TState;
+//   private reducers: ReducerDictionary<TState>;
+//   private listeners = new Set<UnaryFunction<TState, void>>();
 
-  constructor(
-    reducers: ReducerDictionary<TState>,
-    initialState: TState = {} as TState
-  ) {
-    this.reducers = reducers;
-    this.state = this.reduce(initialState, {
-      type: "none",
-    });
-  }
+//   constructor(
+//     reducers: ReducerDictionary<TState>,
+//     initialState: TState = {} as TState
+//   ) {
+//     this.reducers = reducers;
+//     this.state = this.reduce(initialState, {
+//       type: "none",
+//     });
+//   }
 
-  get value(): TState {
-    return this.state;
-  }
+//   get value(): TState {
+//     return this.state;
+//   }
 
-  dispatch(action: Action): void {
-    this.state = this.reduce(this.state, action);
-    this.listeners.forEach((fn) => fn(this.state));
-  }
+//   dispatch(action: Action): void {
+//     this.state = this.reduce(this.state, action);
+//     this.listeners.forEach((fn) => fn(this.state));
+//   }
 
-  private reduce(state: TState, action: Action): TState {
-    const result = Object.entries(this.reducers).reduce(
-      (state, [key, reducer]: any) => ({
-        ...state,
-        [key]: reducer((state as any)[key], action),
-      }),
-      state
-    );
+//   private reduce(state: TState, action: Action): TState {
+//     const result = Object.entries(this.reducers).reduce(
+//       (state, [key, reducer]: any) => ({
+//         ...state,
+//         [key]: reducer((state as any)[key], action),
+//       }),
+//       state
+//     );
 
-    return result;
-  }
+//     return result;
+//   }
 
-  subscribe(fn: (state: TState) => void) {
-    fn(this.state);
-    this.listeners.add(fn);
+//   subscribe(fn: (state: TState) => void) {
+//     fn(this.state);
+//     this.listeners.add(fn);
 
-    return () => {
-      this.listeners.delete(fn);
-    };
-  }
-}
+//     return () => {
+//       this.listeners.delete(fn);
+//     };
+//   }
+// }
 
-export const reducers = {
-  [COUNTER_KEY]: counterReducer,
-};
+// export const reducers = {
+//   [COUNTER_KEY]: counterReducer,
+// };
 
-export const store = new Store<GlobalState>(reducers);
+// export const store = new Store<GlobalState>(reducers);
--- my-app/src/types/index.ts
+++ my-app/src/types/index.ts
@@ -2,3 +2,39 @@ export interface Action {
     type: string
     payload?: object
 }
+
+export type TState = Record<string, unknown>;
+
+export type UnaryFunction<T, R> = (input: T) => R;
+
+export type Reducer<TState, Action> = (state: TState, action: Action) => TState;
+
+export type ReducerDictionary<
+  TState extends Record<K, unknown>,
+  K extends string
+> = Record<K, Reducer<TState[K], Action>>;
+
+export class Store<TState extends Record<K, unknown>, K extends string> {
+  private state: TState;
+  private reducers: ReducerDictionary<TState, K>;
+  private listeners = new Set<UnaryFunction<TState, void>>();
+
+  constructor(reducers: ReducerDictionary<TState, K>, state: TState) {
+    this.reducers = reducers;
+  }
+
+  get value(): TState {
+    return this.state;
+  }
+
+  dispatch(action: Action): void {
+    this.state = this.reduce(this.state, action);
+    this.listeners.forEach((fn) => fn(this.state));
+  }
+
+  reduce(state: TState, action: Action): TState {
+    const result = Object.entries(this.reducers).reduce((state, [key, reduc]));
+  }
+
+  subscribe(): {};
+}
